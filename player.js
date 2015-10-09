32230ac3708ce5fb652878b3549e0fedbb30aab3
George <Gerhut@GMail.com>
2015-10-09 14:15:39 +0800

    restructed player

--- player.js
+++ player.js
@@ -6,24 +6,30 @@ var player = function ($) {
   var BULLET_HEIGHT = 11
   var BULLET_SPEED = .08
 
-  var grade = 0
-
   // Whole Container
   var player = new $.Container
   player.hitters = {}
+
+  function onStageMouseMove(event) {
+    if (!$.Ticker.paused) {
+      hero.x = event.stageX
+      hero.y = event.stageY
+    }
+  }
 
   player.on('added', function () {
     var stage = this.stage
     hero.x = stage.canvas.width / 2
     hero.y = stage.canvas.height * 4 / 5
-    stage.on('stagemousemove', function (event) {
-      if (!$.Ticker.paused) {
-        hero.x = event.stageX
-        hero.y = event.stageY
-      }
-    })
+    hero.gotoAndPlay('flying')
+    stage.addEventListener('stagemousemove', onStageMouseMove)
     shoot()
-  }, null, true)
+
+    player.on('removed', function (event) {
+      hero.stop()
+      stage.removeEventListener('stagemousemove', onStageMouseMove)
+    }, null, true)
+  })
 
   // Hero player
   var hero = new $.Sprite(new $.SpriteSheet({
@@ -49,81 +55,54 @@ var player = function ($) {
     }, null, true)
   })
 
-  hero.on('upgrade', function () {
-    if (grade < 3) {
-      grade += 1
-    }
-  })
-
   player.addChild(hero)
   player.hitters.hero = hero
 
   // Bullet
-  function shoot() {
-    if (hero.currentAnimation != 'flying') {
+  function Bullet() {
+    var bullet = new $.Bitmap('bullet.png')
+
+    bullet.x = hero.x
+    bullet.y = hero.y - HERO_HEIGHT / 2
+    bullet.regX = BULLET_WIDTH / 2
+    bullet.regY = BULLET_HEIGHT
+    bullet.visible = false
+
+    bullet.on('hit', function () {
+      $.Tween.removeTweens(this)
+      Bullet.remove.call(this)
+    }, null, true)
+
+    return bullet
+  }
+
+  Bullet.add = function add() {
+    player.addChild(this)
+    player.hitters['bullet-' + this.id] = this
+  }
+
+  Bullet.remove = function remove() {
+    this.removeAllEventListeners()
+    player.removeChild(this)
+    delete player.hitters['bullet-' + this.id]
+  }
+
+  function shoot() {console.log('shoot')
+    if (hero.currentAnimation != 'flying' || hero.paused) {
       return
     }
 
-    var filename = grade != 0 ? 'bullet1.png' : 'bullet2.png'
-    var trajectories = [
-      [0, 0]
-    ]
+    var bullet = new Bullet()
 
-    if (grade == 2) {
-      trajectories.push(
-        [-HERO_WIDTH / 4, -HERO_WIDTH / 4],
-        [HERO_WIDTH / 4, HERO_WIDTH / 4]
-      )
-    } else if (grade == 3) {
-      trajectories.push(
-        [-HERO_WIDTH / 4, -HERO_WIDTH / 4 - hero.y / 4],
-        [-HERO_WIDTH / 8, -HERO_WIDTH / 8 - hero.y / 8],
-        [HERO_WIDTH / 4, HERO_WIDTH / 4 + hero.y / 4],
-        [HERO_WIDTH / 8, HERO_WIDTH / 8 + hero.y / 8]
-      )
-    }
+    $.Tween.get(bullet, {
+      useTicks: true
+    }).to({ visible: true }, SHOOT_DELAY)
+      .call(shoot)
+      .to({ x: hero.x, y: 0 }, hero.y * BULLET_SPEED)
+      .call(Bullet.remove)
 
-    trajectories.forEach(function (trajectory, index) {
-      var bullet = new $.Bitmap(filename)
-      bullet.regY = BULLET_HEIGHT
-      bullet.visible = false
-      bullet.on('hit', function () {
-        $.Tween.removeTweens(this)
-        destroy.call(this)
-      }, null, true)
-
-      var tween = new $.Tween(bullet, {
-        useTicks: true
-      }).wait(SHOOT_DELAY)
-
-      if (index == 0) {
-        tween.call(shoot)
-      }
-
-      tween
-        .to({ visible: true,
-          x: hero.x + trajectory[0],
-          y: hero.y - HERO_HEIGHT / 2
-        }, 0)
-        .to({
-          x: hero.x + trajectory[1],
-          y: 0
-        }, hero.y * BULLET_SPEED)
-        .call(destroy)
-
-      player.addChild(bullet)
-      player.hitters['bullet-' + bullet.id] = bullet
-    })
-
-    function destroy() {
-      player.removeChild(this)
-      delete player.hitters['bullet-' + this.id]
-    }
+    Bullet.add.call(bullet)
   }
-  //////////// TEST
-  hero.on('click', function () {
-    this.dispatchEvent(new $.Event('upgrade'))
-  })
 
   return player
 } (createjs)
