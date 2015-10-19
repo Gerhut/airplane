8f0d7eee9ae670d2cfb3ebbdbb732d9f2cab587c
qifei <tuqifei111@126.com>
2015-10-19 09:38:39 +0800

    yes

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+/*
+
+	1.加载资源
+	2.通知我机，敌机，用户界面完成初始化
+	3.点击开始按钮，通知敌机我机运动，隐藏控制界面
+	4.点击暂停按钮，通知敌机我机暂停，展示控制界面
+	5.当我机被击落时，或完成所有关卡，游戏结束
+*/
+
+
+
 var stage = new createjs.Stage('stage')
 
-console.log(stage)
+var controller = {
+	start:function (argument) {
+		
+	},
+	pause:function (argument) {
+		
+	},
+	goOn:function (argument) {
+		
+	},
+	end:function (argument) {
+		
+	}
+};
+
+//增加玩家
+stage.addChild(player)
+createjs.Touch.enable(stage)
+createjs.Ticker.timingMode = createjs.Ticker.RAF
+createjs.Ticker.addEventListener('tick', stage)
+
+stage.addEventListener('stagemousedown',function(e){
+	var pause = createjs.Ticker.getPaused();
+	createjs.Ticker.setPaused(!pause);
+
+});
---
+++ player.js
@@ -0,0 +1,133 @@
+var player = function ($) {
+  var HERO_WIDTH = 102
+  var HERO_HEIGHT = 126
+  var HERO_START_X = 1 / 2
+  var HERO_START_Y = 4 / 5
+  var SHOOT_DELAY = 5
+  var BULLET_WIDTH = 5
+  var BULLET_HEIGHT = 11
+  var BULLET_SPEED = .08
+  console.log(2);
+  // Whole Container
+  var player = new $.Container
+  player.hitters = {}
+
+  player.on('added', function () {
+    var stage = this.stage
+    var stageWidth = stage.canvas.width
+    var stageHeight = stage.canvas.height
+
+    hero.x = stageWidth * HERO_START_X
+    hero.y = stageHeight * HERO_START_Y
+
+    hero.gotoAndPlay('flying')
+    stage.addEventListener('stagemousedown', onStageMouseDown)
+    player.hitters.hero = hero
+    shoot()
+
+    player.on('removed', function (event) {
+      stage.removeEventListener('stagemousedown', onStageMouseDown)
+      stage.removeEventListener('stagemousemove', onStageMouseMove)
+      stage.removeEventListener('stagemouseup', onStageMouseUp)
+    }, null, true)
+
+    var lastX, laseY
+    function onStageMouseDown(event) {
+      lastX = event.stageX
+      lastY = event.stageY
+      stage.addEventListener('stagemousemove', onStageMouseMove)
+      stage.addEventListener('stagemouseup', onStageMouseUp)
+    }
+    function onStageMouseMove(event) {
+      if (hero.currentAnimation != 'flying') {
+        return
+      }
+      hero.x = Math.min(Math.max(hero.x + event.stageX - lastX,
+        HERO_WIDTH / 2), stageWidth - HERO_WIDTH / 2)
+      hero.y = Math.min(Math.max(hero.y + event.stageY - lastY,
+        HERO_HEIGHT / 2), stageHeight - HERO_HEIGHT / 2)
+      lastX = event.stageX
+      lastY = event.stageY
+    }
+    function onStageMouseUp(event) {
+      stage.removeEventListener('stagemousemove', onStageMouseMove)
+      stage.removeEventListener('stagemouseup', onStageMouseUp)
+      lastX = lastY = void 0
+    }
+  })
+
+  // Hero player
+  var hero = new $.Sprite(new $.SpriteSheet({
+    images: ['hero.png'],
+    frames: {
+      width: HERO_WIDTH,
+      height: HERO_HEIGHT
+    },
+    animations: {
+      flying: [0, 1, 'flying'],
+      blowup: [2, 5, null, .3]
+    }
+  }))
+
+  hero.regX = HERO_WIDTH / 2
+  hero.regY = HERO_HEIGHT / 2
+
+  hero.on('hit', function () {
+    delete player.hitters.hero
+    hero.gotoAndPlay('blowup')
+    hero.on('animationend', function () {
+      console.log('Game Over!')
+    }, null, true)
+  })
+
+  player.addChild(hero)
+
+  // Bullet
+  function Bullet() {
+    var bullet = new $.Bitmap('bullet.png')
+
+    bullet.x = hero.x
+    bullet.y = hero.y - HERO_HEIGHT / 2
+    bullet.regX = BULLET_WIDTH / 2
+    bullet.regY = BULLET_HEIGHT
+    bullet.visible = false
+
+    bullet.on('added', onBulletAdded)
+    bullet.on('removed', onBulletRemoved)
+    bullet.on('hit', onBulletHit)
+
+    return bullet
+  }
+
+  function onBulletAdded() {
+    player.hitters['bullet-' + this.id] = this
+  }
+
+  function onBulletRemoved() {
+    delete player.hitters['bullet-' + this.id]
+    this.removeAllEventListeners()
+    $.Tween.removeTweens(this)
+  }
+
+  function onBulletHit() {
+    player.removeChild(this)
+  }
+
+  function shoot() {
+    var bullet = new Bullet
+
+    $.Tween.get(bullet, { useTicks: true })
+      // show bullet after SHOOT_DELAY
+      .to({ visible: true }, SHOOT_DELAY)
+      // shoot next bullet if hero is still flying
+      .call(function () { hero.currentAnimation == 'flying' && shoot() })
+      // fly bullet to the top of screen
+      .to({ x: hero.x, y: 0 }, hero.y * BULLET_SPEED)
+      // remove bullet
+      .call(function () { player.removeChild(this) })
+
+    player.addChild(bullet)
+  }
+
+  return player
+} (createjs)
